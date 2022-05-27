1c04274588100907cce6e0ddd87ae5d1f6043987
Aman <77211729+amanisreal@users.noreply.github.com>
2022-05-28 02:37:21 +0530

    Create App.js

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import react from "react";
+import Header from "./componenets/Header"
+import Home from "./componenets/home";
+
+function App(){
+    return(
+      <div className="App">
+          <Header/>
+          <Home/>
+      </div>
+    );
+}
+
+export default App;
