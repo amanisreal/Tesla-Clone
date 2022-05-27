712b640fe8315aab1f70c19b66525f257b643ca7
Aman <77211729+amanisreal@users.noreply.github.com>
2022-05-28 02:42:02 +0530

    Add files via upload

---
+++ src/index.js
@@ -0,0 +1,12 @@
+import React from "react";
+import reactDom from "react-dom";
+import App from "./App";
+import "./index.css";
+
+
+reactDom.render(
+    <div>
+        <App/>
+    </div>,
+    document.getElementById("root")
+)
